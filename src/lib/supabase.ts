6adfc013521f04ab5e2b6b7ea17a9c1e5e77ab4c
syriusintelligence-gif <syriusintelligence@gmail.com>
2025-11-24 09:06:17 -0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -32,6 +32,7 @@ export interface Database {
           created_at: string;
           verified: boolean;
           is_admin: boolean;
+          blocked: boolean;
         };
         Insert: {
           id?: string;
@@ -45,6 +46,7 @@ export interface Database {
           created_at?: string;
           verified?: boolean;
           is_admin?: boolean;
+          blocked?: boolean;
         };
         Update: {
           id?: string;
@@ -58,6 +60,7 @@ export interface Database {
           created_at?: string;
           verified?: boolean;
           is_admin?: boolean;
+          blocked?: boolean;
         };
       };
       signed_contents: {
